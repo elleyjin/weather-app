a114b4a1b858c3fb8fef385c0696ace8e421beee
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-22 12:24:59 +0800

    added bootstrap & skeleton structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container-fluid">
+      <nav className="Navbar navbar">
+        <h1>New York</h1>
+        <form>
+          <input type="search" placeholder="Enter a city"></input>
+          <button className="btn btn-outline-light">Search</button>
+        </form>
+      </nav>
+      <div className="Main">
+        <img
+          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/097/811/original/sun.png?1695302792"
+          alt="weather-icon"
+        />
+        <span className="Temperature">27</span>
+        <span className="Units">°C</span>
+        <span className="Separator"> | </span>
+        <span className="Units">°F</span>
+        <p>l: | h: </p>
+      </div>
+      <footer className="Footer">
+        <p>Open source code by Chan Ee Lin/Elley Jin</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
